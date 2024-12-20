74827a355eb855991448f466b089e9ff4fa867b2
RaghadAbusamor <raghadmohammad253@gmail.com>
2024-12-20 16:33:26 +0200

    Update wsgi.py

--- AbuSammourDrivingSchool/wsgi.py
+++ AbuSammourDrivingSchool/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AbuSammourDrivingSchool.settings')
 
 application = get_wsgi_application()
+
+# Alias `application` as `app` for Vercel compatibility
+app = application
